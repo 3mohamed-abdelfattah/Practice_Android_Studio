26ac7a104d2d07d21c82ba91f006ec4445eda2eb
MohamedMohamed <m01126165016@icloud.com>
2024-05-13 21:29:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/projectone/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.projectone
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projectone.MainActivity.Constant.myName
+import com.example.projectone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // For Get ID
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        callBack()
+        callView()
+
+        Log.d(MainActivity.LOG_TAG, "On Create")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity.LOG_TAG, "On Start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity.LOG_TAG, "On Resume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MainActivity.LOG_TAG, "On Pause")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity.LOG_TAG, "On Restart")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity.LOG_TAG, "On Stop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity.LOG_TAG, "On Destroy")
+
+    }
+
+    companion object {
+        const val LOG_TAG = "FIRST_ACTIVITYYYY"
+    }
+
+    // Go Next Activity
+    private fun callBack() {
+        binding.nextB.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            val name = binding.passData.text.toString()
+            intent.putExtra(myName, name)
+            startActivity(intent)
+        }
+    }
+
+    private fun callView() {
+
+        // Go Link
+
+        binding.nextV.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/3mohamed-abdelfatt")
+            startActivity(intent)
+        }
+
+        // Make Call
+        binding.nextC.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            val phoneNum = binding.getNum.text.toString()
+            intent.data = Uri.parse("tel:$phoneNum")
+            startActivity(intent)
+        }
+    }
+
+
+    // Send Data
+    object Constant {
+        val myName = "Mohamed Mohamed"
+    }
+
+}
---
+++ app/src/main/java/com/example/projectone/MainActivity2.kt
@@ -0,0 +1,77 @@
+package com.example.projectone
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projectone.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+        val name = intent.getStringExtra(MainActivity.Constant.myName)
+        binding.textView2.text = name
+
+        backStep()
+
+        Log.d(MainActivity2.LOG_TAG, "On Create")
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity2.LOG_TAG, "On Start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity2.LOG_TAG, "On Resume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MainActivity2.LOG_TAG, "On Pause")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity2.LOG_TAG, "On Restart")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity2.LOG_TAG, "On Stop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity2.LOG_TAG, "On Destroy")
+
+    }
+
+    companion object {
+        const val LOG_TAG = "Secount_ACTIVITYYYY"
+    }
+
+    // Go Previous Activity
+    private fun backStep() {
+        binding.backB.setOnClickListener {
+            finish()
+        }
+    }
+}
