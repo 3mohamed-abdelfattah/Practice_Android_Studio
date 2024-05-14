d1117ef5e4ae4ea7e876c485f544b6291cf6d8f9
MohamedMohamed <m01126165016@icloud.com>
2024-05-14 06:40:16 +0300

    Change Fragments From Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/projectone/MainActivity3.kt
+++ app/src/main/java/com/example/projectone/MainActivity3.kt
@@ -1,6 +1,7 @@
 package com.example.projectone
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,35 @@ import com.example.projectone.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
 
+    val myFirstFramgment = fragment_One()
+    val mySecoundFramgment = fragment_two()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
 
+        initView()
+        initButton()
     }
 
+    //Show one Fragment
+    private fun initView() {
+        val transection = supportFragmentManager.beginTransaction()
+        transection.add(R.id.FragmentView, myFirstFramgment)
+        transection.commit()
+    }
+
+    //To Get Button And Go Second Fragment
+    private fun initButton() {
+        findViewById<Button>(R.id.back_frag).setOnClickListener {
+            showSecoundFragment()
+        }
+    }
+
+    private fun showSecoundFragment() {
+        val transetion = supportFragmentManager.beginTransaction()
+        transetion.replace(R.id.FragmentView, mySecoundFramgment)
+        transetion.commit()
+    }
 
 }
