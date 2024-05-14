2c290ae72b75b7926800b918fb56007c44bd3429
MohamedMohamed <m01126165016@icloud.com>
2024-05-15 01:52:47 +0300

    Collapsing Toolbar

---
+++ app/src/main/java/com/example/projectone/ScrollingActivity.kt
@@ -0,0 +1,28 @@
+package com.example.projectone
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectone.databinding.ActivityScrollingBinding
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+}
