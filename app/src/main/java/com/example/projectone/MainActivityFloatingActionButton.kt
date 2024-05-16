0a4145578c833d341ab81e241bd6f16dd7926a0e
MohamedMohamed <m01126165016@icloud.com>
2024-05-16 10:02:21 +0300

    Alert Dialog

--- app/src/main/java/com/example/projectone/MainActivityFloatingActionButton.kt
+++ app/src/main/java/com/example/projectone/MainActivityFloatingActionButton.kt
@@ -1,8 +1,10 @@
 package com.example.projectone
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,7 +13,7 @@ import com.example.projectone.databinding.ActivityMainFloatingActionButtonBindin
 class MainActivityFloatingActionButton : AppCompatActivity() {
     lateinit var binding: ActivityMainFloatingActionButtonBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityMainFloatingActionButtonBinding.inflate(layoutInflater)
+        binding = ActivityMainFloatingActionButtonBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
@@ -21,8 +23,39 @@ class MainActivityFloatingActionButton : AppCompatActivity() {
             insets
         }
 
-        binding.floatingActionButton.setOnClickListener{
-            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+        binding.floatingActionButton.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
+
+        binding.alert.setOnClickListener {
+            showAlert()
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.apply {
+            setTitle("Location Permission")
+            setMessage("Accept this permission..?")
+            setPositiveButton(
+                "Accept",
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    Toast.makeText(
+                        this@MainActivityFloatingActionButton,
+                        "Done..!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                })
+            setNegativeButton(
+                "Cancel",
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    Toast.makeText(
+                        this@MainActivityFloatingActionButton,
+                        "Canceled..!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                })
+        }
+        val dialog = builder.create().show()
     }
 }
